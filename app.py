d22257f652541f1eec39430548228ef62e68d197
yeldos-umbetov <yeleke.umbetov@gmail.com>
2021-06-22 09:31:25 +0600

    fixes for Heroku

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flas
+import pickle
 
+from flask import Flask, request, jsonify
+from model_files.slogan_model import generate, load_tokenizer_and_model
 
-app = Flask(__name__)
+app = Flask('ibec_robot')
 
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# generate
+@app.route("/bot", methods=['POST'])
+def hellobot():
+    company = request.args.get('company')
+    industry = request.args.get('industry')
+    # product = request.args.get('product')
+
+    result_text = "Компания: " + company + ". Индустрия: " + industry + ". Слоган: "
+
+    tok, model = load_tokenizer_and_model("sberbank-ai/rugpt3small_based_on_gpt2")
+    generated = generate(model, tok, result_text, num_beams=10)
+    generated_text = generated[0]
+
+    return generated_text
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=9696)
